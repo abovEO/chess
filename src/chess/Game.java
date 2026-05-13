768b9bb75bc97e4f1dd084a2193fd701fc8b4927
abovEO <dasnainsi@gmail.com>
2026-05-13 08:38:15 +0530

    added game and main logic

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -8,6 +8,37 @@ public class Game {
     private Player CurrentPlayer;
 
     private Board board;
+    private final MovesValidator validator;
 
+    public Game() {
+        board = new Board();
 
+        WhitePlayer = new Player("white", Color.WHITE);
+        BlackPlayer = new Player("black", Color.BLACK);
+
+        CurrentPlayer = WhitePlayer;
+        validator = new MovesValidator();
+    }
+
+    public void makeMove(int fromRow, int fromCol, int toRow, int toCol) {
+        Cell from = board.getCell(fromRow, fromCol);
+        Cell to = board.getCell(toRow, toCol);
+
+        Move move = new Move(from, to);
+
+        boolean isValidMove = validator.isValidMove(board, move, CurrentPlayer);
+
+        if (isValidMove) {
+            board.movePiece(from, to);
+            switchTurns();
+            System.out.println("Move Executed");
+        } else {
+            System.out.println("Invalid move");
+            return;
+        }
+    }
+
+    public void switchTurns() {
+        CurrentPlayer = CurrentPlayer == WhitePlayer ? BlackPlayer : WhitePlayer;
+    }
 }
